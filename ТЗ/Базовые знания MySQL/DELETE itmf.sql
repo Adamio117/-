8c7dc2d1181ccfde371e1598891509dab9efcc1b
Adamio117 <136378040+Adamio117@users.noreply.github.com>
2025-11-26 18:58:51 +0300

    Delete ТЗ directory

--- ТЗ/Базовые знания MySQL/DELETE itmf.sql
+++
@@ -1,8 +0,0 @@
-DELETE itmf
-FROM
-    indicator_to_mo_fact itmf
-    JOIN indicator_to_mo itm ON itmf.indicator_to_mo_id = itm.indicator_to_mo_id
-    JOIN indicator i ON itm.indicator_id = i.indicator_id
-WHERE
-    i.indicator_id = 273
-    AND DATE (itmf.fact_time) = '2024-09-10';
